0b84e4b2fe64445865e540ea2444ffbd0bf35612
christineaboylan <christine.boylan3@gmail.com>
2016-09-01 20:56:09 -0400

    class07 work

---
+++ class07/class07/first-javascript/script.js
@@ -0,0 +1,26 @@
+alert("Hi my name is:Christine");
+
+document.write('this is some text');
+
+document.write(100/10);
+document.write('<br>');
+document.write(50-17);
+document.write(50+17);
+document.write("Christine"+" "+"Boylan")
+
+var num1= 10*10;
+
+document.write(num1);
+
+console.log(num1);
+
+
+//function
+
+function doSomething(){
+	console.log('this is working');
+}
+
+//call the function
+
+doSomething();
