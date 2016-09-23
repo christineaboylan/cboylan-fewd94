0220eb875ceceda8c11db0ddfddbfe1b0b159863
christineaboylan <christine.boylan3@gmail.com>
2016-09-22 20:45:31 -0400

    class11 homework

--- class11/class11/temp-converter/js/app.js
+++ class11/class11/temp-converter/js/app.js
@@ -1,11 +1,26 @@
-//$('#submit').click(function(){
-  alert(parseInt($('#fname').val()) + 10);
-});
-
-
 $(document).ready(function(){
+console.log('hi')
 
+var rawTemp;
+var convTemp;
+var rawUnit;
+var convUnit;
 
+$('#submit').click(function(e){
+	rawTemp = $('#input').val();
+	rawUnit = $('select').val();
+	if(rawUnit === 'f'){
+		convTemp = (rawTemp - 32) / 1.8;
+		convUnit = 'c';
+	} else{
+			convTemp = (rawTemp *1.8) + 32;
+			convUnit = 'f';
+		}
+		$('#output').html(convTemp + " " + convUnit);
+e.preventDefault()
+});
 
 });
+
+
 
